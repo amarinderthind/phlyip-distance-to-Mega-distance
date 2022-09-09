0ce1b2cea91d935b660d87e530d56ede3bd739cc
Amarinder <thind.amarinder@gmail.com>
2022-09-09 17:17:11 +1000

    Update distane_matrix_conversion.r

--- distane_matrix_conversion.r
+++ distane_matrix_conversion.r
@@ -7,10 +7,12 @@ names1 <- dist1$X1
 dist1 <- dist1[,-1]
 
 dist1[upper.tri(dist1,diag=TRUE)] <- NA  #lower.tri
+dist_mega <- sapply(as.data.frame(dist_mega), as.character)
+dist_mega[is.na(dist_mega)] <- " "
 
 lin1 <- paste0("#mega","\n","!Title: Concatenated Files;", "\n","!Format DataType=Distance DataFormat=LowerLeft;","\n", sep = '')
 
 write.table(lin1,"new_distance1.meg", col.names = FALSE, row.names = FALSE,quote = FALSE)
 write.table(paste('#',names1, sep = ''),"new_distance1.meg", sep="\t",append = TRUE, col.names = FALSE, row.names = FALSE,quote = FALSE)
 write.table("\n","new_distance1.meg", sep="\t",append = TRUE, col.names = FALSE, row.names = FALSE,quote = FALSE)
-write.table(mt,"new_distance1.meg", sep="\t", append = TRUE,col.names = FALSE, row.names = FALSE)
+write.table(mt,"new_distance1.meg", sep="\t", append = TRUE,col.names = FALSE, row.names = FALSE,quote = FALSE)
